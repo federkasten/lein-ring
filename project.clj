b786120ef2d77cce8dd39f3cf934583fbfe101f5
Takashi AOKI <federkasten@me.com>
2014-09-24 14:29:45 +0900

    fix document

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-ring-jetty "0.1.0-SNAPSHOT"
-  :description "Leiningen Ring plugin base on jetty"
-  :url ""
+  :description "Leiningen Ring-Jetty plugin"
+  :url "https://github.com/federkasten/lein-ring-jetty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
